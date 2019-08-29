07e81cd42282e1bff8041690562fe3b5b1a46898
claytonschoonoversmith@gmail.com <claytonsmith@Claytons-MacBook-Pro.local>
2019-08-29 02:11:22 -0500

    signup route connected to mongoDB and creating users via postman

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-	console.log(req.body);
-	res.send('You made a POST request')
+router.post('/signup', async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = new User({ email, password });
+	await user.save();
+
+	res.send('You made a POST request');
+
+	// console.log(`\n this is req.body: , ${email, password}`);
+
 });
 
 module.exports = router;
